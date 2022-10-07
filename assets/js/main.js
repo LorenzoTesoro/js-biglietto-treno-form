4a0ecc779e58eaeb45c2fa55df60dcef56c73406
LorenzoTesoro <lorenzotesoro2@gmail.com>
2022-10-07 17:10:37 +0200

    add main sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,13 @@
-
 document.querySelector("button").addEventListener("click", function() {
     
-    let kilometers = document.getElementById("km").value;
-    let userAge = document.getElementById("age").value;
-    let price = (kilometers * 0.21);
+    let kilometers = document.getElementById("km").value; //km
+    let userAge = document.getElementById("age").value;//age
+    let price = (kilometers * 0.21); //cost
 
     if(userAge < 18){
-        price =(price - (price / 100) * 20).toFixed(2);
+        price =(price - (price / 100) * 20).toFixed(2); //underage discount
     } else if (userAge >= 65){
-        price =(price - (price / 100) * 40).toFixed(2);
+        price =(price - (price / 100) * 40).toFixed(2); //senior discount
     }
-
-    
+    document.getElementById("costo_biglietto").innerHTML = price + " €";
 })
